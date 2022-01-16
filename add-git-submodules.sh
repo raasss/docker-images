244d3b1f616a43c64418071ac53d3ffa96aff12c
Radisa Jovicevic <radisa@gmail.com>
2022-01-16 09:06:22 +0100

    add helper script for add all submodules

---
+++ add-git-submodules.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for DISTRIBUTION in ubuntu-20.04 ubuntu-20.10 ubuntu-21.04 ubuntu-21.10 debian-10 debian-11; do
+	for PACKAGE in apache php-fpm mariadb mysql; do
+		git submodule add git@github.com:raasss/docker-${PACKAGE}-${DISTRIBUTION}.git ${PACKAGE}/${DISTRIBUTION}
+	done
+done
