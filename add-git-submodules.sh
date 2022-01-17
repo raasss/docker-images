331a3282c63813c9673117f80bb3610685e206d2
Radisa Jovicevic <radisa@gmail.com>
2022-01-17 09:17:25 +0100

    add centos-7 distribution to supported list of docker images

--- add-git-submodules.sh
+++ add-git-submodules.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for DISTRIBUTION in ubuntu-20.04 ubuntu-20.10 ubuntu-21.04 ubuntu-21.10 debian-10 debian-11; do
+for DISTRIBUTION in ubuntu-20.04 ubuntu-20.10 ubuntu-21.04 ubuntu-21.10 debian-10 debian-11 centos-7; do
 	for PACKAGE in apache php-fpm mariadb mysql; do
 		git submodule add git@github.com:raasss/docker-${PACKAGE}-${DISTRIBUTION}.git ${PACKAGE}/${DISTRIBUTION}
 	done
